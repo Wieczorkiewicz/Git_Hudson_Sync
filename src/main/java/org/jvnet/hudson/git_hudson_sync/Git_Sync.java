8aaeff6fd02d756e9eb4c2c0852b203ac74f98b2
wlicpsc <wlicpsc@gmail.com>
2010-11-09 10:44:22 -0800

    Removed redundant imports

--- src/main/java/org/jvnet/hudson/git_hudson_sync/Git_Sync.java
+++ src/main/java/org/jvnet/hudson/git_hudson_sync/Git_Sync.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
-
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
